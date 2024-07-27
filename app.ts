bd9b2ba22e462789ca586e22beb99dea312ec776
ryan anderson <ryanmanderson1986@gmail.com>
2024-07-27 12:57:07 -0500

    basic crud with zod

--- app.ts
+++ app.ts
@@ -1,6 +1,14 @@
 import { Hono } from 'hono'
+import {logger} from 'hono/logger'
+
+import { expensesRoute } from './routes/expenses'
+
 const app = new Hono()
 
+app.use('*', logger()) // add logger middleware
+
 app.get('/', (c) => c.text('Hono!'))
+
+app.route('/api/expenses', expensesRoute)
 
 export default app
---
+++ routes/expenses.ts
@@ -0,0 +1,48 @@
+import { Hono } from 'hono'
+import { zValidator } from '@hono/zod-validator'
+import { z } from 'zod'
+
+type Expenses = {
+    id: number,
+    title: string,
+    amount: number
+}
+
+let fakeExpenses: Expenses[] = [
+    {id: 1, title: "title 1", amount: 100},
+    {id: 2, title: "title 2", amount: 200},
+    {id: 3, title: "title 3", amount: 300},
+]
+
+const createPostSchema = z.object({
+    title: z.string().min(3).max(100),
+    amount: z.number().int().positive()
+})
+
+export const expensesRoute = new Hono()
+.get('/', async (c) => {
+    return c.json({expenses: fakeExpenses})
+})
+.post('/', zValidator("json", createPostSchema), async (c) => {
+    const expense = await c.req.valid("json")
+    fakeExpenses.push({...expense, id: fakeExpenses.length+1})
+    return c.json(fakeExpenses)
+})
+.get('/:id{[0-9]+}', (c) => {
+    const id = Number.parseInt(c.req.param('id'))
+    const expense = fakeExpenses.find(expense => expense.id === id)
+    if(!expense) {
+        return c.notFound()
+    }
+    return c.json(expense)
+})
+.delete('/:id{[0-9]+}', (c) => {
+    const id = Number.parseInt(c.req.param('id'))
+    const index = fakeExpenses.findIndex(expense => expense.id === id)
+    if(index === -1) {
+        return c.notFound()
+    }
+    const deletedExpense = fakeExpenses.splice(index, 1)[0]
+    return c.json({expense: deletedExpense})
+})
+//.put
