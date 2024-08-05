c265bcac6a95027e87b1faa259ae6891e9c6bcc9
ryan anderson <ryanmanderson1986@gmail.com>
2024-08-04 20:02:53 -0500

    added create expense form with ts form

--- frontend/src/routes/create-expense.tsx
+++ frontend/src/routes/create-expense.tsx
@@ -1,5 +1,130 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute, useNavigate } from '@tanstack/react-router'
+
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import { Button } from '@/components/ui/button'
+
+import { useForm } from '@tanstack/react-form'
+import { api } from '@/lib/api'
 
 export const Route = createFileRoute('/create-expense')({
-  component: () => <div>Hello /create-expense!</div>
+  component: CreateExpense
 })
+
+function CreateExpense() {
+  const navigate = useNavigate()
+  
+  const form = useForm({
+    defaultValues: {
+      title: '',
+      amount: 0,
+    },
+    onSubmit: async ({ value }) => {
+      const res = await api.expenses.$post({json: value })
+      if(!res.ok) {
+        throw new Error('error adding new expense')
+      }
+      navigate({to:'/expenses'})
+    },
+  })
+
+  return (
+    <div className='p-4 max-w-3xl mx-auto'>
+      <h1>Create Expense</h1>
+      <form className='max-w-xl m-auto' 
+        onSubmit={(e) => {
+          e.preventDefault()
+          e.stopPropagation()
+          form.handleSubmit()
+        }}>
+        
+        <form.Field
+          name="title"
+          /* validators={{
+            onChange: ({ value }) =>
+              !value
+                ? 'A first name is required'
+                : value.length < 3
+                  ? 'First name must be at least 3 characters'
+                  : undefined,
+            onChangeAsyncDebounceMs: 500,
+            onChangeAsync: async ({ value }) => {
+              await new Promise((resolve) => setTimeout(resolve, 1000))
+              return (
+                value.includes('error') &&
+                'No "error" allowed in first name'
+              )
+            },
+          }} */
+          children={(field) => {
+            // Avoid hasty abstractions. Render props are great!
+            return (
+              <>
+                <Label htmlFor={field.name}>Title</Label>
+                <Input
+                  id={field.name}
+                  name={field.name}
+                  type="text"
+                  value={field.state.value}
+                  onBlur={field.handleBlur}
+                  onChange={(e) => field.handleChange(e.target.value)}
+                />
+                {field.state.meta.isTouched && field.state.meta.errors.length ? (
+                  <em>{field.state.meta.errors.join(", ")}</em>
+                ) : null}
+                {field.state.meta.isValidating ? 'Validating...' : null}
+              </>
+            )}
+          }
+        />
+        <form.Field
+          name="amount"
+          /* validators={{
+            onChange: ({ value }) =>
+              !value
+                ? 'A first name is required'
+                : value.length < 3
+                  ? 'First name must be at least 3 characters'
+                  : undefined,
+            onChangeAsyncDebounceMs: 500,
+            onChangeAsync: async ({ value }) => {
+              await new Promise((resolve) => setTimeout(resolve, 1000))
+              return (
+                value.includes('error') &&
+                'No "error" allowed in first name'
+              )
+            },
+          }} */
+          children={(field) => {
+            // Avoid hasty abstractions. Render props are great!
+            return (
+              <>
+                <Label htmlFor={field.name}>Amount</Label>
+                <Input
+                  id={field.name}
+                  name={field.name}
+                  type="number"
+                  value={field.state.value}
+                  onBlur={field.handleBlur}
+                  onChange={(e) => field.handleChange(Number(e.target.value))}
+                />
+                {field.state.meta.isTouched && field.state.meta.errors.length ? (
+                  <em>{field.state.meta.errors.join(", ")}</em>
+                ) : null}
+                {field.state.meta.isValidating ? 'Validating...' : null}
+              </>
+            )}
+          }
+        />
+        <form.Subscribe
+          selector={(state) => [state.canSubmit, state.isSubmitting]}
+          children={([canSubmit, isSubmitting]) => (
+            <Button type="submit" className='mt-4' disabled={!canSubmit}>
+              {isSubmitting ? 'Submitting Expense' : 'Create Expense'}
+            </Button>
+          )}
+        />
+      </form>
+    </div>
+  )
+}
--- frontend/src/routes/expenses.tsx
+++ frontend/src/routes/expenses.tsx
@@ -1,5 +1,76 @@
 import { createFileRoute } from '@tanstack/react-router'
+import { api } from '@/lib/api'
+import { useQuery } from '@tanstack/react-query'
+
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table"
+
+import { Skeleton } from "@/components/ui/skeleton"
+
+type Expense = {
+  id: number,
+  title: string,
+  amount: number
+}
 
 export const Route = createFileRoute('/expenses')({
-  component: () => <div>Hello /expenses!</div>
+  component: Expenses
 })
+
+async function getAllExpenses() {
+  const res = await api.expenses.$get()
+  if(!res.ok) {
+    throw new Error("server error")
+  }
+  const data = await res.json()
+  return data 
+}
+
+function Expenses() {
+  const { isPending, error, data } = useQuery({ queryKey: ['get-all-expenses'], queryFn: getAllExpenses })
+
+  if (error) return 'An error has occurred: ' + error.message
+
+  return (
+    <div className='p-4 max-w-3xl mx-auto'>
+      <Table>
+        <TableCaption>A list of your recent expenses.</TableCaption>
+        <TableHeader>
+          <TableRow>
+            <TableHead className="w-[100px]">ID</TableHead>
+            <TableHead>Expense</TableHead>
+            <TableHead>Amount</TableHead>
+            {/* <TableHead>Method</TableHead>
+            <TableHead className="text-right">Amount</TableHead> */}
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {isPending
+          ? Array(3).fill(0).map((_, i) => (
+            <TableRow key={i}>
+              <TableCell><Skeleton className='h-4' /></TableCell>
+              <TableCell><Skeleton className='h-4' /></TableCell>
+              <TableCell><Skeleton className='h-4' /></TableCell>
+            </TableRow>
+          ))
+          : data?.expenses.map((expense: Expense) => (
+            <TableRow key={expense.id}>
+              <TableCell>{expense.id}</TableCell>
+              <TableCell>{expense.title}</TableCell>
+              <TableCell>{expense.amount}</TableCell>
+              {/* <TableCell>{invoice.paymentMethod}</TableCell>
+              <TableCell className="text-right">{invoice.totalAmount}</TableCell> */}
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </div>
+  )
+}
