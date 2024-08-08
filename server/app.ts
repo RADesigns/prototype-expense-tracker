44ff5032e977bb1fbffcf0ef097f3aa8c34f7ec5
ryan anderson <ryanmanderson1986@gmail.com>
2024-08-08 10:59:36 -0500

    /me route return getUserProfile. Next: refactor to middleware

--- server/app.ts
+++ server/app.ts
@@ -2,6 +2,7 @@ import { Hono } from 'hono'
 import {logger} from 'hono/logger'
 
 import { expensesRoute } from './routes/expenses'
+import { authRoute } from './routes/auth'
 
 import { serveStatic } from 'hono/bun'
 
@@ -9,7 +10,7 @@ const app = new Hono()
 
 app.use('*', logger()) // add logger middleware
 
-const apiRoutes = app.basePath('/api').route('/expenses', expensesRoute)
+const apiRoutes = app.basePath('/api').route('/expenses', expensesRoute).route('/', authRoute)
 
 app.get('*', serveStatic({ root: './frontend/dist' }))
 app.get('*', serveStatic({ path: './frontend/dist/index.html' }))
--- server/kinde.ts
+++ server/kinde.ts
@@ -1,4 +1,6 @@
 import {createKindeServerClient, GrantType, type SessionManager} from "@kinde-oss/kinde-typescript-sdk";
+import { type Context } from "hono";
+import { getCookie, setCookie, deleteCookie } from "hono/cookie";
 
 // Client for authorization code flow
 export const kindeClient = createKindeServerClient(GrantType.AUTHORIZATION_CODE, {
@@ -11,17 +13,29 @@ export const kindeClient = createKindeServerClient(GrantType.AUTHORIZATION_CODE,
 
 let store: Record<string, unknown> = {};
 
-export const sessionManager: SessionManager = {
-  async getSessionItem(key: string) {
-    return store[key];
-  },
-  async setSessionItem(key: string, value: unknown) {
-    store[key] = value;
-  },
-  async removeSessionItem(key: string) {
-    delete store[key];
-  },
-  async destroySession() {
-    store = {};
-  }
-};
+export const sessionManager = (c: Context): SessionManager => ({
+    async getSessionItem(key: string) {
+      const result = getCookie(c, key);
+      return result;
+    },
+    async setSessionItem(key: string, value: unknown) {
+      const cookieOptions = {
+        httpOnly: true,
+        secure: true,
+        sameSite: "Lax",
+      } as const;
+      if (typeof value === "string") {
+        setCookie(c, key, value, cookieOptions);
+      } else {
+        setCookie(c, key, JSON.stringify(value), cookieOptions);
+      }
+    },
+    async removeSessionItem(key: string) {
+      deleteCookie(c, key);
+    },
+    async destroySession() {
+      ["id_token", "access_token", "user", "refresh_token"].forEach((key) => {
+        deleteCookie(c, key);
+      });
+    },
+  });
